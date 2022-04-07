149512b193b159c2770dec8ebb6b27a14e3653ad
Maikon <m.carvalho@grouplinknetwork.com>
2022-04-07 15:00:30 -0300

    init the project and create first contract

---
+++ contracts/schoolNFT.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyToken is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    mapping(string => uint8) existingURI;
+
+
+    constructor() ERC721("MyToken", "MTK") {
+
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function isContentOwned(string memory uri) public view returns (bool) {
+        return existingURI[uri] == 1;
+    }
+
+    function payToMint(
+        address recipient,
+        string memory metaDataURI
+
+    ) public payable returns (uint256) {
+        require(existingURI[metaDataURI] != 1, "URI already exists");
+    
+    }
+
+    }
+
+}
